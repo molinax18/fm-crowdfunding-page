9072bdeebb7bfcf517b49c06bee91564dda9fd9b
molinax18 <arielmolina8953@gmail.com>
2026-09-07 16:39:40 -0300

    feat(App): implement HeroBackground component chore(style): remove bg-gray-300 style from body

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import HeroBackground from "./components/HeroBackground";
 
 export default function App() {
   return (
     <div className="grid w-[90%] mx-auto max-w-300">
+      <HeroBackground />
       <Header />
     </div>
   );

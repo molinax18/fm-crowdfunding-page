37404c8143f7834ab99b883e440ed8b450d929fe
molinax18 <arielmolina8953@gmail.com>
2026-09-06 23:59:19 -0300

    feat(constants): new links file

---
+++ src/constants/links.ts
@@ -0,0 +1,19 @@
+interface Link {
+  href: string;
+  value: string;
+}
+
+export const LINKS: Link[] = [
+  {
+    href: "#",
+    value: "About",
+  },
+  {
+    href: "#",
+    value: "Discover",
+  },
+  {
+    href: "#",
+    value: "Get Started",
+  },
+];

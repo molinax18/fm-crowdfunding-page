34f53a78984a2d022e7c1845d9e010beb7f7840d
molinax18 <arielmolina8953@gmail.com>
2026-09-07 00:01:03 -0300

    feat(Header): new menu state and implement NavbarMobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import MenuIcon from "../assets/svg/icon-hamburger.svg?react";
+import CloseIcon from "../assets/svg/icon-close-menu.svg?react";
+import NavbarMobile from "./NavbarMobile";
+
 import crowdfundLogo from "../assets/svg/logo.svg";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => setOpen((prev) => !prev);
+
   return (
-    <header className="flex items-center justify-between py-6">
+    <header className="relative z-10 flex items-center justify-between py-6">
       <img src={crowdfundLogo} alt="Crowdfund logo" />
 
-      <MenuIcon />
+      {open ? (
+        <CloseIcon onClick={toggleMenu} />
+      ) : (
+        <MenuIcon onClick={toggleMenu} />
+      )}
+
+      {open && <NavbarMobile />}
     </header>
   );
 }

0ddf101889b8342a7f4bcb3bc0266fb0e0a273cd
molinax18 <arielmolina8953@gmail.com>
2026-09-14 00:30:23 -0300

    chore: remove unused file

--- src/context/CrowdfundContext.ts
+++
@@ -1,4 +0,0 @@
-import type { ICrowdfundContext } from "../types/crowdfundContext";
-import { createContext } from "react";
-
-export const CrowdfundContext = createContext<ICrowdfundContext | null>(null);

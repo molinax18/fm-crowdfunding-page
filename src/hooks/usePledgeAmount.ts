bcbdf82bf3711501a1f825fb0fa8896b320a76b6
molinax18 <arielmolina8953@gmail.com>
2026-09-13 21:41:53 -0300

    chore(usePledgeAmount): isValid max number

--- src/hooks/usePledgeAmount.ts
+++ src/hooks/usePledgeAmount.ts
@@ -3,7 +3,8 @@ import { useState } from "react";
 export function usePledgeAmount(defaultValue?: string) {
   const [amount, setAmount] = useState(defaultValue ?? "");
   const amountToNumber = Number(amount.trim());
-  const isValid = amountToNumber >= Number(defaultValue ?? 0);
+  const isValid =
+    amountToNumber >= Number(defaultValue ?? 0) && amountToNumber <= 999;
 
   return {
     amount,
